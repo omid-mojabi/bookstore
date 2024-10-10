a06dd0f54f5485ec1d2115127ec1bfd2594cb255
Omid Mojabi <46277874+omid-mojabi@users.noreply.github.com>
2024-10-10 17:50:05 +0200

    Delete src/main/java/repository directory

--- src/main/java/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package bookstore.repository;
-
-import bookstore.model.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem,Long> {
-}
--- src/main/java/repository/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package bookstore.repository;
-
-import bookstore.model.entity.ShoppingCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-}
